6257b99ec6fde0f74981801bb3a1e3af043dc13f
Alex <Qwertylex@gmail.com>
2014-03-18 17:02:03 -0400

    Update domain-updater.sh

--- domain-updater.sh
+++ domain-updater.sh
@@ -6,16 +6,20 @@ CheckIP2(){
 IP_Old=$(curl -s -4 "http://checkip.dns.he.net/")
 }
 UpdateIP() {
-(curl -s -4 "http://DOMAIN-NAME-GOES-HERE:UPDATE-KEY-GOES-HERE@dyn.dns.he.net/nic/update?hostname=DOMAIN-NAME-GOES-HERE" 2>&1 | sed "s/^/`date +'%a %b %d %r'` /" && echo) >> dynlog/ip-updates.log
+(curl -s -4 "http://DOMAIN-GOES-HERE:UPDATE-KEY-GOES-HERE@dyn.dns.he.net/nic/update?hostname=DOMAIN-GOES-HERE" 2>&1 | sed "s/^/`date +'%a %b %d %r'` /" && echo) >> dynlog/ip-updates.log
 }
 echo "Domain-Updater Started."
-while :
-do
-CheckIP
-if [ "$IP" != "$IP_Old" ]
-then
-CheckIP; CheckIP2; UpdateIP
-fi
-trap "echo Domain-Updater Stopped.;exit 0" SIGINT SIGTERM
-sleep 1200
+while true; do
+        CheckIP;
+        echo "`date +'%a %b %d %r'` Checking..."
+        if [ "$IP" != "$IP_Old" ]; then
+                CheckIP;
+                CheckIP2;
+                UpdateIP;
+        fi
+        else
+        echo "`date +'%a %b %d %r'` No change, all good." >> dynlog/ip-updates.log
+        echo "`date +'%a %b %d %r'` No change, all good."
+        trap "echo Domain-Updater Stopped.;exit 0" SIGINT SIGTERM
+        sleep 3555
 done
